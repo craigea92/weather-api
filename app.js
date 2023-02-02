807689315da5da9655d17362177bd868cdb890b4
Craig Eaton <craigea92@gmail.com>
2023-02-02 16:51:09 +0000

    Added API url to app.js and console logged the result, status is 200

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require("express");
+const https = require("https");
 
 const app = express();
+const url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=8e129600d05a9783c82678a5689c521d"
 
 app.get("/", function(req, res){
+  https.get(url, function(response){
+    console.log(response)
+  });
+
   res.send("Server is up and running")
 })
 
